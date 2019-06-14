173430d01a56ff018cd15b683496bf5936db9145
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2019-06-14 13:50:50 -0500

    #2555 Printing Orders with Jasper Reports do not work anymore https://github.com/adempiere/adempiere/issues/2555

--- base/src/org/compiere/print/ServerReportCtl.java
+++ base/src/org/compiere/print/ServerReportCtl.java
@@ -41,7 +41,7 @@ public class ServerReportCtl {
 	public static boolean startDocumentPrint (int type, MPrintFormat customPrintFormat, int recordId, String printerName , ProcessInfo processInfo)
 	{
 		String trxName;
-		if (processInfo != null)
+		if (processInfo != null && processInfo.getTransactionName() != null)
 			trxName = processInfo.getTransactionName();
 		else
 			trxName = null;
@@ -96,7 +96,7 @@ public class ServerReportCtl {
      */
 	public static boolean runJasperProcess(int recordId, ReportEngine reportEngine, boolean isDirectPrint, String printerName, ProcessInfo processInfo) {
 		Trx trx;
-		if (processInfo != null)
+		if (processInfo != null && processInfo.getTransactionName() != null)
 			trx = Trx.get(processInfo.getTransactionName() , false);
 		else
 			trx = null;
